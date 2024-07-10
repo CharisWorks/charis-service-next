92958b44d29fc9a424e8df65b88fe83ea289d7e3
Myxogastria0808 <r.rstudio.c@gmail.com>
2024-07-10 15:19:20 +0900

    feat: remove mui

--- src/app/_components/card.tsx
+++ src/app/_components/card.tsx
@@ -1,43 +1,35 @@
 import * as React from "react";
-import Box from "@mui/material/Box";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
 import { MeiliItemHit } from "@/api/meili";
-
 import Image from "next/image";
-import { Grid } from "@mui/material";
 
 export default function ItemCard(props: { item: MeiliItemHit }) {
   return (
-    <Card sx={{ minWidth: 275 }}>
-      <CardContent>
-        <Grid container alignItems={"center"} justifyContent={"center"}>
-          <Grid>
+    <div>
+      <div>
+        <div>
+          <div>
             <Image
               src={props.item.thumbnail_url}
               width={200}
               height={200}
               alt="Picture of the author"
             />
-          </Grid>
-        </Grid>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          </div>
+        </div>
+        <div>
           {props.item.genre}
-        </Typography>
-        <Typography variant="h5" component="div">
+        </div>
+        <div>
           {props.item.item_name}
-        </Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+        </div>
+        <div>
           {props.item.worker}
-        </Typography>
-        <Typography variant="body2">{props.item.price}</Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Details</Button>
-      </CardActions>
-    </Card>
+        </div>
+        <div>{props.item.price}</div>
+      </div>
+      <div>
+        <button>Details</button>
+      </div>
+    </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,7 @@
 import React, { useState } from "react";
 import { Checkout } from "@/api/gobackend";
 import { MeiliItemHit, Search } from "@/api/meili";
-import { Container } from "@mui/material";
 import ItemCard from "./_components/card";
-import Grid from "@mui/material/Grid";
 export default function Home() {
   const [keyword, setKeyword] = useState("");
   const [itemId, setItemId] = useState(0);
@@ -12,7 +10,7 @@ export default function Home() {
   const [items, setItems] = useState<MeiliItemHit[]>([]);
   return (
     <>
-      <Container maxWidth="lg">
+      <div>
         <input
           type="text"
           onChange={(e) => {
@@ -30,60 +28,16 @@ export default function Home() {
         >
           検索！！
         </button>
-        <Grid container spacing={2}>
+        <div>
           {items.map((item, key) => {
             return (
-              <Grid item xs={4} key={key}>
+              <div key={key}>
                 <ItemCard item={item} />
-              </Grid>
+              </div>
             );
           })}
-        </Grid>
-      </Container>
-
-      {/* <h1>Search</h1>
-      <div>
-        <input
-          type="text"
-          onChange={(e) => {
-            setKeyword(e.target.value);
-          }}
-        />
-        <button
-          onClick={async () => {
-            const { data } = await Search([keyword]);
-            console.log(data);
-          }}
-        >
-          検索！！
-        </button>
+        </div>
       </div>
-      <div>
-        <h1>Checkout</h1>
-        <div>商品ID</div>
-        <input
-          type="text"
-          onChange={(e) => {
-            setItemId(parseInt(e.target.value));
-          }}
-        />
-        <div>数量</div>
-        <input
-          type="number"
-          onChange={(e) => {
-            setQuantity(Number(e.target.value));
-          }}
-        />
-
-        <button
-          onClick={async () => {
-            const { data } = await Checkout({ itemId, quantity });
-            console.log(data?.url);
-          }}
-        >
-          購入！！
-        </button>
-      </div> */}
     </>
   );
 }
