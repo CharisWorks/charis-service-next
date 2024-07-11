48e54c637bac152e95de25770675a0ab077698be
WhatACotton <whatacotton@gmail.com>
2024-07-11 12:25:52 +0900

    add: card

--- src/app/_components/Card/index.tsx
+++ src/app/_components/Card/index.tsx
@@ -2,40 +2,79 @@ import * as React from "react";
 import { MeiliItemHit } from "@/api/meili";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { css } from "../../../../styled-system/css";
 
 const ItemCard = (props: { item: MeiliItemHit }) => {
   const router = useRouter();
+  const card = css({
+    bgColor: "night",
+    width: "260px",
+    padding: "2",
+  });
 
   return (
-    <div>
+    <div className={card}>
       <div>
-        <div>
+        <button
+          onClick={() => {
+            router.push(`item/${props.item.id}`);
+          }}
+        >
           <div>
-            <Image
-              src={props.item.thumbnail_url}
-              width={200}
-              height={200}
-              alt="Picture of the author"
-            />
+            <div
+              className={css({
+                display: "flex",
+                justifyContent: "center",
+              })}
+            >
+              <Image
+                src={props.item.thumbnail_url}
+                width={"150"}
+                height={"150"}
+                alt="Picture of the author"
+                className={css({
+                  borderRadius: "50%",
+                  width: "150px",
+                  height: "150px",
+                })}
+              />
+            </div>
           </div>
-        </div>
-        <div>
-          {props.item.genre}
-        </div>
-        <div>
-          {props.item.item_name}
-        </div>
-        <div>
-          {props.item.worker}
-        </div>
-        <div>{props.item.price}</div>
-      </div>
-      <div>
-        <button onClick={() => { router.push(`item/${props.item.id}`) }}>Details</button>
+          <div
+            className={css({
+              fontSize: "12px",
+              color: "star",
+              display: "flex",
+              justifyContent: "flex-end",
+            })}
+          >
+            {props.item.worker}
+          </div>
+          <div
+            className={css({
+              fontSize: "16px",
+              fontWeight: 700,
+              color: "star",
+              display: "flex",
+              justifyContent: "flex-start",
+            })}
+          >
+            ￥{props.item.price}
+          </div>
+          <div
+            className={css({
+              width: "240px",
+              fontSize: "24px",
+              fontWeight: 700,
+              color: "star",
+            })}
+          >
+            {props.item.item_name}
+          </div>
+        </button>
       </div>
     </div>
   );
-}
+};
 
 export default ItemCard;
-
